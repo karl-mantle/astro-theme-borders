e702b107fc54526a07032513508ffda1083ba516
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2025-01-01 20:57:06 +0000

    fix: updated astro & site configs

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,19 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
+// site
 export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE_SEPARATOR = '-';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_SHORT_TITLE = 'A blog';
+export const SITE_FOOTER_TEXT = 'Copyright - All rights reserved.';
+
+// posts
+export const POSTS_PER_PAGE = 3;
+export const PERMALINK_BLOG = 'blog';
+export const PERMALINK_PROJECTS = 'projects';
+
+// brand
+export const SITE_LOGO_SVG_SQUARE = './assets/logo/svg/Logo-1-1.svg';
+
+// i18n
+export const SITE_LANGUAGE = 'en-US';
+export const DATE_FORMAT = 'j F Y';
+export const TIME_FORMAT = 'g:i A';
