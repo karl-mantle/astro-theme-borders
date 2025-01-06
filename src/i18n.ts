b9262eff0d4623feaab7d0304f86dc8fef64bfd9
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2025-01-06 19:16:54 +0000

    feat: date time localisation

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,30 +1,19 @@
-import { SITE_LANGUAGE, DATE_FORMAT, TIME_FORMAT } from '../consts';
-
-const suffixes: { [key: string]: string[] } = {
-  'en': ['th', 'st', 'nd', 'rd'],
-  // to do
-};
-
-// this possibly isn't worth pursuing yet now as won't apply to many non-IE languages, also RTL scripts?
-const getSuffix = (day: number, locale: string) => {
-  const lang = locale.split('-')[0];
-  const suffixSet = suffixes[lang] || suffixes['en'];
-
-  if (day > 3 && day < 21) return suffixSet[0];
-  switch (day % 10) {
-    case 1: return suffixSet[1];
-    case 2: return suffixSet[2];
-    case 3: return suffixSet[3];
-    default: return suffixSet[0];
-  }
-};
+// https://www.php.net/manual/en/datetime.format.php
+import { SITE_LANGUAGE, DATE_FORMAT, TIME_FORMAT } from './consts';
 
 const formatMapping: {[key: string]: (date: Date, locale: string) => string } = {
   d: date => String(date.getDate()).padStart(2, '0'),
   j: date => String(date.getDate()),
-  S: (date, locale) => { // needs looking at if useful or not
+  S: (date) => {
     const day = date.getDate();
-    return getSuffix(day, locale);
+    const suffixSet = ['th', 'st', 'nd', 'rd']
+    if (day > 3 && day < 21) return suffixSet[0];
+    switch (day % 10) {
+      case 1: return suffixSet[1];
+      case 2: return suffixSet[2];
+      case 3: return suffixSet[3];
+      default: return suffixSet[0];
+    }
   },
   l: (date, locale) => date.toLocaleDateString(locale, { weekday: 'long' }),
   D: (date, locale) => date.toLocaleDateString(locale, { weekday: 'short' }),
@@ -42,13 +31,13 @@ const formatMapping: {[key: string]: (date: Date, locale: string) => string } = 
   H: date => String(date.getHours()).padStart(2, '0'),
   i: date => String(date.getMinutes()).padStart(2, '0'),
   s: date => String(date.getSeconds()).padStart(2, '0'),
-  T: date => date.toLocaleTimeString('en-US', { timeZoneName: 'short' }).split(' ')[2], // to do multilingual timezones, why not
+  T: (date, locale) => date.toLocaleTimeString(locale, { timeZoneName: 'short' }).split(' ')[2],
   c: date => date.toISOString(),
   r: date => date.toUTCString(),
   U: date => String(Math.floor(date.getTime() / 1000))
 };
 
-export function getFormattedDate(date: string): string {
+export function formatDateTime(date: string): string {
   const dateTime = new Date(date);
   const replaceDateFormat = (match: string): string => (formatMapping[match] ? formatMapping[match](dateTime, SITE_LANGUAGE) : match);
 
