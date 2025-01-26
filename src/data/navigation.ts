8491b8bb0ac74ff890d37c3ae5bc0e9b583143e2
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2025-01-26 20:34:36 +0000

    fix: navigation and posts fixes

---
+++ src/data/navigation.ts
@@ -0,0 +1,102 @@
+export const headerNavigation = [
+  {
+    "id": "cG9zdDoyMTE",
+    "parentId": null,
+    "slug": "blog",
+    "label": "Blog"
+  },
+  {
+    "id": "cG9zdDoyMTE",
+    "parentId": null,
+    "slug": null,
+    "label": "Sample Pages"
+  },
+  {
+    "id": "cG9zdDoyNw",
+    "parentId": null,
+    "slug": null,
+    "label": "Components"
+  },
+  {
+    "id": "cG9zdDoyMjM",
+    "parentId": "cG9zdDoyNw",
+    "slug": null,
+    "label": "Cards"
+  },
+  {
+    "id": "cG9zdDoyOA2",
+    "parentId": "cG9zdDoyNw",
+    "slug": null,
+    "label": "Entries"
+  },
+  {
+    "id": "cG9zdDo4Mg",
+    "parentId": "cG9zdDoyNw",
+    "slug": "components/partials",
+    "label": "Partials"
+  },
+  {
+    "id": "cG9zdDoyMjI",
+    "parentId": "cG9zdDoyNw",
+    "slug": null,
+    "label": "Sections"
+  },
+  {
+    "id": "cG9zdDoyMTI",
+    "parentId": "cG9zdDoyNw",
+    "slug": "components/ui",
+    "label": "UI Components"
+  },
+  {
+    "id": "cG9zdqyMjM",
+    "parentId": "cG9zdDoyMjM",
+    "slug": "components/card/content",
+    "label": "Content"
+  },
+  {
+    "id": "cG9zdDo4Mw",
+    "parentId": "cG9zdDoyMjM",
+    "slug": "components/card/features",
+    "label": "Features"
+  },
+  {
+    "id": "cG9zdDoyMTQ",
+    "parentId": "cG9zdDoyMjM",
+    "slug": "components/card/statistics",
+    "label": "Statistics"
+  },
+  {
+    "id": "cG9zdDoya22",
+    "parentId": "cG9zdDoyOA2",
+    "slug": "components/collection/posts",
+    "label": "Posts type"
+  },
+  {
+    "id": "cG9zdDoyMTc",
+    "parentId": "cG9zdDoyMjI",
+    "slug": "components/section/cta-banner",
+    "label": "CTA Banner"
+  },
+  {
+    "id": "cG9zdDoyOA",
+    "parentId": "cG9zdDoyMjI",
+    "slug": "components/section/hero-banner",
+    "label": "Hero Banner"
+  },
+  {
+    "id": "cG9zdDoyMTM",
+    "parentId": "cG9zdDoyMjI",
+    "slug": "components/section/image-banner",
+    "label": "Image Banner"
+  },
+  {
+    "id": "cGyidDoyMTM",
+    "parentId": "cG9zdDoyMjI",
+    "slug": "components/section/gallery-banner",
+    "label": "Gallery Banner"
+  }
+];
+
+export default {
+  headerNavigation
+};
